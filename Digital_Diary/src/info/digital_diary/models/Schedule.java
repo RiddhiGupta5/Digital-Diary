0aeaf5884e6c7d6e63ab6bb8d3a7d5cd60268b5e
divyanshmmantri <56482570+divyanshmmantri@users.noreply.github.com>
2019-10-31 12:48:13 +0530

    Add files via upload

--- Digital_Diary/src/info/digital_diary/models/Schedule.java
+++ Digital_Diary/src/info/digital_diary/models/Schedule.java
@@ -1,5 +1,55 @@
-package info.digital_diary.models;
+package info.digitaldiary.models;
 
-public class Schedule {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Pattern;
 
+public class Schedule{
+	
+	 private String useremailid;
+	 private String date;
+	 private String Activity;
+	 private String time;
+	 
+	 public String getuseremailid() {
+		 return useremailid;
+	 }
+	 public void setuseremailid(String useremailid) {
+		 if (!Pattern.matches("^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$", useremailid)) {
+				throw new IllegalArgumentException("Invalid Email Address");
+		 }
+		 this.useremailid=useremailid;
+	 }
+	 public String getDate() {
+		 return date;
+	 }
+	 public void setDate(String date) {
+		 if (date.equals("default"))
+			{
+				DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+				LocalDateTime now = LocalDateTime.now();
+				this.date = dtf.format(now);
+			}
+			else
+			{
+				this.date = date;
+			}
+		}
+	public String getActivity() {
+		return Activity;
+	}
+	public void setActivity(String Activity) {
+		this.Activity=Activity;
+	}
+	public String gettime(){
+		return time;
+		
+	}
+	public void settime(String time)
+	{
+		this.time=time;
+	}
+	 
+		 
+	 
 }
